ba254abc1f213227eed2c0a7142f3aa0b551f441
vld7wn <52673140+vld7wn@users.noreply.github.com>
2021-06-01 00:29:21 +0300

    v2

--- ATToolls/src/test/java/TestCalculater.java
+++ ATToolls/src/test/java/TestCalculater.java
@@ -7,29 +7,73 @@ public class TestCalculater {
     @DataProvider
     public Object[][] positive() {
         return new Object[][]{
-                {5, 6, 11, '+'}, {40, 50, 90, '+'},
-                {66, 84, 150, '+'}
+                {5, 6, 11, "+"}, {40, 50, 90, "+"},
+                {66, 84, 150, "+"}
         };
     }
 
     @DataProvider
     public Object[][] negative() {
         return new Object[][]{
-                {6, 18, '+'}, {47, 5, 90, '+'},
-                {8, 11, '+'},
-                {Float.MAX_VALUE, 40, '+'},
-                {Float.MIN_VALUE, 40, '-'}
+                {55, 2, "+"},
+                {4, "58fa", "/"},
+                {2, "qq", "*"},
+                {"*", "*", "*"},
+                {4, 2, "ff85"},
+                {Double.MAX_VALUE, 40, '+'},
+                {Double.MIN_VALUE, 40, '-'}
         };
     }
 
+    @Test   //positive
+    public void randomTestSmallNum() {
+        String[] operators = new String[]{"+", "-", "*", "/"};
+        for (int i = 0; i < 20; i++) {
+            float a = randFloat(0, 50);
+            float b = randFloat(1, 50);
+            String op = operators[(int) randFloat(0, 3)];
+            Assert.assertEquals(solution(a, b, op), new Calculater().calculate(a, b, op));
+            System.out.println(a + " " + op + " " + b + " = " + solution(a, b, op));
+        }
+    }
+
+    @Test   //positive
+    public void randomTestBigNum() {
+        String[] operators = new String[]{"+", "-", "*", "/"};
+        for (int i = 0; i < 20; i++) {
+            float a = randFloat(0, 9999);
+            float b = randFloat(1, 9999);
+            String op = operators[(int) randFloat(0, 3)];
+            Assert.assertEquals(solution(a, b, op), new Calculater().calculate(a, b, op));
+            System.out.println(a + " " + op + " " + b + " = " + solution(a, b, op));
+        }
+    }
+
     @Test(dataProvider = "positive")
-    public void testPositive(float first, float second, float equals, char action) {
-        Assert.assertEquals(equals, new Calculater().calculate(first, second, action));
+    public void testPositive(float a, float b, float equals, String operator) {
+        System.out.println(a + " " + operator + " " + b);
+        Assert.assertEquals(equals, new Calculater().calculate(a, b, operator));
     }
 
     @Test(dataProvider = "negative", expectedExceptions = {ClassCastException.class,
             NullPointerException.class, AssertionError.class})
-    public void testNegative(Object first, Object second, Object action) {
-        Assert.assertFalse((Boolean) new Calculater().calculate(first, second, action));
+    public void testNegative(Object a, Object b, Object operator) {
+        System.out.println(a + " " + operator + " " + b);
+        Assert.assertFalse((Boolean) new Calculater().calculate( operator, a, b));
+    }
+
+
+    private static float randFloat(float min, float max) {
+        return (float) (min + Math.random() * ((max - min) + 1));
+    }
+
+    private static float solution(float a, float b, String operator) {
+        if (operator.equals("+"))
+            return a + b;
+        if (operator.equals("-"))
+            return a - b;
+        if (operator.equals("*"))
+            return a * b;
+        return a / b;
     }
 }
